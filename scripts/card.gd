139b09f47a0c80329221633ba21e9474b183d5d2
gonzapiacen <gonzapiacentini@gmail.com>
2026-05-23 16:19:51 -0300

    Card and Effect classes

--- scripts/card.gd
+++ scripts/card.gd
@@ -1,2 +1,7 @@
 extends Resource
 class_name Card
+
+enum Type{Instant, Permanent}
+@export var cost: int
+@export var effects: Array[Effect]
+@export var type: Type
---
+++ scripts/effect_info.gd
@@ -0,0 +1,5 @@
+extends Resource
+class_name EffectInfo
+
+@export var name: String
+@export var description: String
